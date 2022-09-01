1f755efb64cef60090080e97af8040a3d9514bce
Tamires <4serimat@gmail.com>
2022-09-01 17:37:33 -0300

    add(app): repositório genérico

---
+++ src/IRepositorioGenerico.java
@@ -0,0 +1,13 @@
+import java.util.List;
+
+public interface IRepositorioGenerico<T> {
+
+    void inserir(T obj) throws ElementoJaExisteException;
+
+    List<T> listar();
+
+    void remover(T obj) throws ElementoNaoExisteException;
+
+    void atualizar(T newObj) throws ElementoNaoExisteException;
+
+}
