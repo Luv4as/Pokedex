4ab126e2b9cf5537cf6151ab2e1ad085a5e691a0
tucoff <baracho.arturraffael@gmail.com>
2022-09-15 13:57:43 -0300

    add: fxml

--- src/scenes/App.java
+++ src/scenes/App.java
@@ -1,11 +1,9 @@
 package scenes;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class App extends Application {
@@ -14,25 +12,20 @@ public class App extends Application {
         launch(args);
     }
 
+    /* (non-Javadoc)
+     * @see javafx.application.Application#start(javafx.stage.Stage)
+     */
     @Override
     public void start(Stage primaryStage) throws Exception {
        
-        //Código não oficial abaixo
-        //####################################################
-        primaryStage.setTitle("Treinador Screen");
-        Button botao = new Button("Clique Aqui");
-        botao.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.print("Clicou no Botao");
-            }
-        });
-        StackPane root = new StackPane();
-        root.getChildren().addAll(botao);
-        primaryStage.setScene(new Scene(root, 400, 500));
+        //Tela login
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("layoutLogin.fxml"));
+        Parent root = fxmlLoader.load();
+        Scene tela = new Scene(root);
+        primaryStage.setTitle("Login");
+        primaryStage.setScene(tela);
         primaryStage.show();
-        //#####################################################
-    
+        
     }
 
 }
