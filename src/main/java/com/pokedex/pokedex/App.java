46aaed5878e14c9098bdcc8738927a950cc782de
Lucas Venancio <lucas.venancio@outlook.com>
2022-09-16 16:50:07 -0300

    arq: Adicionei o layoutUsuario

--- src/main/java/com/pokedex/pokedex/App.java
+++ src/main/java/com/pokedex/pokedex/App.java
@@ -19,7 +19,7 @@ public class App extends Application {
     public void start(Stage primaryStage) throws Exception {
 
         //Tela login
-        FXMLLoader loader = new FXMLLoader(App.class.getResource("layoutLogin.fxml"));
+        FXMLLoader loader = new FXMLLoader(App.class.getResource("layoutUsuario.fxml"));
         Parent root = loader.load();
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
