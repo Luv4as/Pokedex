c32938cd814fe847761422bfaab4f1601547c8b4
Lucas Venancio <lucas.venancio@outlook.com>
2022-09-17 12:37:26 -0300

    bugfix: concertei o layoutUsuario

--- src/main/java/com/pokedex/pokedex/App.java
+++ src/main/java/com/pokedex/pokedex/App.java
@@ -18,7 +18,7 @@ public class App extends Application {
     public void start(Stage primaryStage) throws Exception {
 
         //Tela login
-        FXMLLoader loader = new FXMLLoader(this.getClass().getResource("fxml/layoutLogin.fxml"));
+        FXMLLoader loader = new FXMLLoader(this.getClass().getResource("fxml/layoutUsuario.fxml"));
         Parent root = loader.load();
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
