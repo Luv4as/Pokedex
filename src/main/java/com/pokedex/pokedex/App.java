d6e82489c1f2dfa433532f46332c96b2d3237204
Lucas Venancio <lucas.venancio@outlook.com>
2022-09-22 20:42:08 -0300

    tirei e coloquei um arquivo

--- src/main/java/com/pokedex/pokedex/App.java
+++ src/main/java/com/pokedex/pokedex/App.java
@@ -17,12 +17,19 @@ public class App extends Application {
 
     public void start(Stage primaryStage) throws Exception {
 
-        //Tela login
-        FXMLLoader loader = new FXMLLoader(App.class.getResource("fxml/layoutLogin.fxml"));
+                //Tela login
+        FXMLLoader loader = new FXMLLoader(App.class.getResource("adilson_fabiano/filho_de_adilson_fabianio.json"));
         Parent root = loader.load();
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
         primaryStage.setResizable(false);
+
+//        //Tela login
+//        FXMLLoader loader = new FXMLLoader(App.class.getResource("fxml/layoutLogin.fxml"));
+//        Parent root = loader.load();
+//        primaryStage.setScene(new Scene(root));
+//        primaryStage.show();
+//        primaryStage.setResizable(false);
 
     }
 
